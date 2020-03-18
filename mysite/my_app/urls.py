bf51cd74eb9b29d29ba005970071b28386ebac72
John Ochs <john.w.ochs0@gmail.com>
2020-03-18 16:36:01 -0500

    gitingore

--- mysite/my_app/urls.py
+++ mysite/my_app/urls.py
@@ -1,4 +1,7 @@
 from . import views
 from django.urls import path, include
 
-urlpatterns = [path("", views.home, name = "start.html"), path("new-search/", views.new_search, name = "new_search")]
+urlpatterns = [
+    path("", views.home, name = "start.html"),
+    path("new-search/", views.new_search, name = "new_search")
+]
